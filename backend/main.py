3ee4e04ff80bfe05ed73bc75d84a495a062044c0
adrian_s_lps <adriansoareslps@gmail.com>
2023-02-05 11:16:05 -0300

    Start fast api project

---
+++ backend/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.post('/check/')
+async def hirschberg_check(edit_params):
+    s_x = edit_params['s1']
+    s_y = edit_params['s2']
+    alphabet = edit_params['alpha']
+    gap_cost = edit_params['gap']
+
+    return controller.hirschberg(s_x, s_y, alphabet, gap_cost)
